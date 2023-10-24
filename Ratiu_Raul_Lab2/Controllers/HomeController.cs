4cdef5f6bc98665ffcd832fdcf7c9eb8a887d92b
raulratiu9 <raulratiu2@gmail.com>
2023-10-24 16:08:21 +0300

    feat: add Statistics view and update HomeController

--- Ratiu_Raul_Lab2/Controllers/HomeController.cs
+++ Ratiu_Raul_Lab2/Controllers/HomeController.cs
@@ -1,22 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ratiu_Raul_Lab2.Data;
 using Ratiu_Raul_Lab2.Models;
+using Ratiu_Raul_Lab2.Models.LibraryViewModels;
 using System.Diagnostics;
 
 namespace Ratiu_Raul_Lab2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+
 
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
 
         public IActionResult Privacy()
         {
---
+++ Ratiu_Raul_Lab2/Models/LibraryViewModels/OrderGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Ratiu_Raul_Lab2.Models.LibraryViewModels
+
+{
+    public class OrderGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? OrderDate { get; set; }
+        public int BookCount { get; set; }
+
+    }
+}
